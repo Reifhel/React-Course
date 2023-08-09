c883cc24fdc7dd6b951098a06189ab8436d3dff1
Reifhel <rafaelherdt1@gmail.com>
2023-08-09 14:06:49 -0300

    Section 5 (Aula 15)

--- Expenses_Controler/src/App.js
+++ Expenses_Controler/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Expenses from './Components/Expenses/Expenses';
+import NewExpense from './Components/NewExpense/NewExpense';
 
 function App() {
 
@@ -28,6 +29,7 @@ function App() {
 
   return (
     <div>
+      <NewExpense />
       <Expenses list={expenses}/>
     </div>
   );
