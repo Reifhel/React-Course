c980fb0663bdd7dbcfe21414c08941ddbf586af5
Reifhel <rafaelherdt1@gmail.com>
2023-08-28 12:16:08 -0300

    Section 9 - Projeto 2(FINALIZADO)

--- Projeto de Adicionar usuarios/src/App.js
+++ Projeto de Adicionar usuarios/src/App.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState } from "react";
 
+import AddUser from "./Components/User/AddUser";
+import UserList from "./Components/User/UserList";
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (username, age) => {
+
+    setUsersList((prevState) => {
+      return [...prevState, { username: username, age: age, id: Math.random().toString() }];
+    });
+
+  };
+
   return (
     <div>
-
+      <AddUser onAddUser={addUserHandler} />
+      <UserList users={usersList} />
     </div>
   );
 }
