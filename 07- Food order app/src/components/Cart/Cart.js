4aec548ba2ad753225ab8a854c492e03e0666e04
Reifhel <rafaelherdt1@gmail.com>
2023-10-03 14:18:45 -0300

    Section 12 - Adicionando o carrinho de compras

---
+++ 07- Food order app/src/components/Cart/Cart.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+import Modal from "../UI/Modal";
+
+import styles from "./Cart.module.css";
+
+function Cart(props) {
+  const cartItems = (
+    <ul className={styles["cart-items"]}>
+      {[{ id: "c1", name: "Sushi", amount: 2, price: 2.0 }].map((item) => (
+        <li>{item.name}</li>
+      ))}
+    </ul>
+  );
+
+  return (
+    <Modal>
+      {cartItems}
+      <div className={styles.total}>
+        <span>Valor Total</span>
+        <span>00.00</span>
+      </div>
+      <div className={styles.actions}>
+        <button className={styles["button--alt"]}>Fechar</button>
+        <button className={styles.button}> Pedir </button>
+      </div>
+    </Modal>
+  );
+}
+
+export default Cart;
