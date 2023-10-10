d72c000658286618dce85b3a72f9d0b423fa72ed
Reifhel <rafaelherdt1@gmail.com>
2023-10-10 11:45:49 -0300

    Section 12 - Criando o context para o carrinho

--- 07- Food order app/src/App.js
+++ 07- Food order app/src/App.js
@@ -1,8 +1,9 @@
-import React, { Fragment, useState } from "react";
+import React, { useState } from "react";
 
 import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import CartProvider from "./store/CartProvider";
 
 function App() {
   const [cartIsShow, setCartisShow] = useState(false);
@@ -16,13 +17,13 @@ function App() {
   };
 
   return (
-    <Fragment>
+    <CartProvider>
       {cartIsShow && <Cart onHideCart={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
--- 07- Food order app/src/components/Layout/HeaderCartButton.js
+++ 07- Food order app/src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,24 @@
-import React from "react";
+import React, { useContext } from "react";
+
+import CartContext from "../../store/cart-context";
 
 import CartIcon from "../Cart/CartIcon";
 import styles from "./HeaderCartButton.module.css";
 
 function HeaderCartButton(props) {
+  const CartCtx = useContext(CartContext);
+
+  const NumberOfCartItems = CartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
+
   return (
     <button className={styles.button} onClick={props.onClick}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
       <span> Seu Carrinho </span>
-      <span className={styles.badge}> 3 </span>
+      <span className={styles.badge}> {NumberOfCartItems} </span>
     </button>
   );
 }
