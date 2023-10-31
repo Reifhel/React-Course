e4f8c76901ca52158559051eb1911d04ed6735e8
Reifhel <rafaelherdt1@gmail.com>
2023-10-31 15:11:21 -0300

    Seção 3: React Essentials - Componentes, JSX, Props, State...

--- 01 React Essentials/src/App.jsx
+++ 01 React Essentials/src/App.jsx
@@ -1,11 +1,48 @@
+import CoreConcept from "./Components/CoreConcept";
 import Header from "./Components/Header";
+import TabButton from "./Components/TabButton";
+
+import CORE_CONCEPTS from "./data.js";
 
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              img={CORE_CONCEPTS[0].image}
+            />
+            <CoreConcept
+              title={CORE_CONCEPTS[1].title}
+              description={CORE_CONCEPTS[1].description}
+              img={CORE_CONCEPTS[1].image}
+            />
+            <CoreConcept
+              title={CORE_CONCEPTS[2].title}
+              description={CORE_CONCEPTS[2].description}
+              img={CORE_CONCEPTS[2].image}
+            />
+            <CoreConcept
+              title={CORE_CONCEPTS[3].title}
+              description={CORE_CONCEPTS[3].description}
+              img={CORE_CONCEPTS[3].image}
+            />
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton> Components </TabButton>
+            <TabButton> JSX </TabButton>
+            <TabButton> Props </TabButton>
+            <TabButton> State </TabButton>
+          </menu>
+        </section>
       </main>
     </div>
   );
