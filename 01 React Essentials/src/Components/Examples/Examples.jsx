1b9ff4c0b29e66131aa239e9861887b44d95764e
Reifhel <rafaelherdt1@gmail.com>
2023-11-13 08:56:53 -0300

    Adicionando o novo projeto e renomeando o antigo

--- 01 React Essentials/src/Components/Examples/Examples.jsx
+++ 01 React Essentials/src/Components/Examples/Examples.jsx
@@ -1,6 +1,7 @@
 import React, { Fragment, useState } from "react";
 import { EXAMPLES } from "../../data";
 import TabButton from "../TabButton";
+import Tabs from "../Tabs";
 import Section from "../UI/Section";
 import ExampleItem from "./ExampleItem";
 
@@ -17,23 +18,33 @@ function Examples() {
   return (
     <Fragment>
       <Section id="examples" title="Examples">
-        <menu>
-          <TabButton onSelect={() => handleSelect("components")}>
-            Components
-          </TabButton>
-          <TabButton onSelect={() => handleSelect("jsx")}> JSX </TabButton>
-          <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
-          <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
-        </menu>
+        <Tabs
+          ButtonsContainer="menu"
+          buttons={
+            <>
+              <TabButton onSelect={() => handleSelect("components")}>
+                Components
+              </TabButton>
+              <TabButton onSelect={() => handleSelect("jsx")}> JSX </TabButton>
+              <TabButton onSelect={() => handleSelect("props")}>
+                Props
+              </TabButton>
+              <TabButton onSelect={() => handleSelect("state")}>
+                State
+              </TabButton>
+            </>
+          }
+        >
+          {!selectedTopic && <p> Please select a topic. </p>}
+          {selectedTopic && (
+            <ExampleItem
+              title={EXAMPLES[selectedTopic].title}
+              description={EXAMPLES[selectedTopic].description}
+              code={EXAMPLES[selectedTopic].code}
+            />
+          )}
+        </Tabs>
       </Section>
-      {!selectedTopic && <p> Please select a topic. </p>}
-      {selectedTopic && (
-        <ExampleItem
-          title={EXAMPLES[selectedTopic].title}
-          description={EXAMPLES[selectedTopic].description}
-          code={EXAMPLES[selectedTopic].code}
-        />
-      )}
     </Fragment>
   );
 }
