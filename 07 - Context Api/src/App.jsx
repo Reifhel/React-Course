ccb36ef05f16ff28cc159667fa00c5bfe22d56f0
Reifhel <rafaelherdt1@gmail.com>
2024-02-26 13:56:10 -0300

    Adicionando context api

--- 07 - Context Api/src/App.jsx
+++ 07 - Context Api/src/App.jsx
@@ -1,8 +1,12 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import Header from './components/Header.jsx';
-import Shop from './components/Shop.jsx';
-import { DUMMY_PRODUCTS } from './dummy-products.js';
+import Header from "./components/Header.jsx";
+import Product from "./components/Product.jsx";
+import Shop from "./components/Shop.jsx";
+
+import { DUMMY_PRODUCTS } from "./dummy-products.js";
+
+import { CartContext } from "./store/shop-cart-context.jsx";
 
 function App() {
   const [shoppingCart, setShoppingCart] = useState({
@@ -65,14 +69,25 @@ function App() {
     });
   }
 
+  const CtxValue = {
+    items: shoppingCart.items,
+    addItemToCart: handleAddItemToCart,
+  };
+
   return (
-    <>
+    <CartContext.Provider value={CtxValue}>
       <Header
         cart={shoppingCart}
         onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
       />
-      <Shop onAddItemToCart={handleAddItemToCart} />
-    </>
+      <Shop>
+        {DUMMY_PRODUCTS.map((product) => (
+          <li key={product.id}>
+            <Product {...product} onAddToCart={handleAddItemToCart} />
+          </li>
+        ))}
+      </Shop>
+    </CartContext.Provider>
   );
 }
 
